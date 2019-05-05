9082258890a117c1dd4e509cec8837f64b3f6eba
Sylvain <sylvoz75@gmail.com>
2019-05-05 15:09:32 +1000

    postLayout page query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return new Promise((resolve, reject) => {
--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from './layout';
 
-class postLayout extends Component {
-  render() {
-    return (
-      <Layout>
-        <h1>postLayout here</h1>
-      </Layout>
-    );
+const postLayout = ({ data }) => {
+  const { markdownRemark } = data;
+  return (
+    <Layout>
+      <h1>{markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+    </Layout>
+  );
+};
+
+// query gets ran through createPages in gatsby-node, https://www.gatsbyjs.org/docs/query-behind-the-scenes/
+// ! means required argument
+/* $slug is passed through the context @see gatsby-node
+  `context: {
+    slug: node.frontmatter.slug,
+  }`
+  It a gatsby feature not graphql, in Apollo we have to physically pass the argument
+ */
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: {
+      slug: {
+        eq: $slug
+      }
+    }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
   }
-}
+`;
 
 export default postLayout;
